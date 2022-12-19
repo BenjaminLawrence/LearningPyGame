999a5292c0ff71014c0ea4f90e4bfc8ec841508c
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2022-12-19 11:56:06 +0800

    Add default screen properties
    Added a white fill and "Game" label to the display screen.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from pygame.locals import *
+import colors
 
 pygame.init()
 
@@ -10,6 +11,8 @@ SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
 
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+DISPLAYSURF.fill(colors.WHITE)
+pygame.display.set_caption("Game")
 
 # Game loop
 while True:
