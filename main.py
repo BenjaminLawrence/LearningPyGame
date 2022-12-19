92573cc0d088944b27735605b7e2229b3e05129c
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2022-12-19 12:59:58 +0800

    Add player and enemy to game loop
    Added a player and an enemy into the game loop. The loop will run the
    player's update function and the enemy's move function before each of
    them run their update function.

--- main.py
+++ main.py
@@ -14,10 +14,20 @@ DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
 
+player = Player()
+enemy1 = Enemy()
+
 # Game loop
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    player.update()
+    enemy1.move()
+
+    DISPLAYSURF.fill(WHITE)
+    player.draw(DISPLAYSURF)
+    enemy1.draw(DISPLAYSURF)
+
     pygame.display.update()
