f783ddb3a120c37c303e5bef8fa3bda7081116d6
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2022-12-19 12:20:59 +0800

    Create enemy.py
    Create enemy.py to handle enemies. So far it contains the constructor, a
    move function, and a draw function.

---
+++ enemy.py
@@ -0,0 +1,18 @@
+import pygame, random
+from screen import SCREEN_WIDTH
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Enemy.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+    def move(self):
+        self.rect.move_ip(0, 10)
+        if (self.rect.bottom > 600):
+            self.rect.top = 0
+            self.rect.center = (random.randint(30, 370), 0)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
