1b2bf240fc0da403ac8edb1b310209c40a3bb93a
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2022-12-19 12:39:35 +0800

    Create player.py
    Created player.py to represent the player. Currently has constructor and
    update function.

---
+++ player.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.locals import *
+from screen import SCREEN_WIDTH
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+
+    def update(self):
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
