b22fb7f9b04f02ba62d21aacf3d656916d3e0ea9
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2022-12-19 11:52:22 +0800

    Create colors.py
    Created colors.py to hold constants for colors separately.

---
+++ colors.py
@@ -0,0 +1,8 @@
+import pygame
+
+BLACK = pygame.Color(0, 0, 0)
+BLUE = pygame.Color(0, 0, 255)
+GREEN = pygame.Color(0, 255, 0)
+GREY = pygame.Color(128, 128, 128)
+RED = pygame.Color(255, 0, 0)
+WHITE = pygame.Color(255, 255, 255)
--- main.py
+++ main.py
@@ -8,13 +8,6 @@ FPS.tick(60)
 
 DISPLAYSURF = pygame.display.set_mode((300, 300))
 
-BLACK = pygame.Color(0, 0, 0)
-BLUE = pygame.Color(0, 0, 255)
-GREEN = pygame.Color(0, 255, 0)
-GREY = pygame.Color(128, 128, 128)
-RED = pygame.Color(255, 0, 0)
-WHITE = pygame.Color(255, 255, 255)
-
 # Game loop
 while True:
     for event in pygame.event.get():
