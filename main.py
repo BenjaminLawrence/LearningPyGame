d44900ffb3fc4133cb6b01a7bf438a2c5b460c97
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2022-12-19 11:31:11 +0800

    Add colors
    Added variables for black, white, grey, and red.

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ pygame.init()
 
 DISPLAYSURF = pygame.display.set_mode((300, 300))
 
+colorBlack = pygame.Color(0, 0, 0)
+colorWhite = pygame.Color(255, 255, 255)
+colorGrey = pygame.Color(128, 128, 128)
+colorRed = pygame.Color(255, 0, 0)
+
 # Game loop
 while True:
     for event in pygame.event.get():
