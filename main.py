fab91df75ab35196710dfd0652487f0a377fb967
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2022-12-19 12:06:47 +0800

    Create screen.py
    Created screen.py to hold constants related to the screen.

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ pygame.init()
 FPS = pygame.time.Clock()
 FPS.tick(60)
 
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 600
-
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 DISPLAYSURF.fill(colors.WHITE)
 pygame.display.set_caption("Game")
